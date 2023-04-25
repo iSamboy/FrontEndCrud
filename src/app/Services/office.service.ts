b1e467054bca8f3069594fc797a537808e78534b
iSamboy <samueldjx@gmail.com>
2023-04-25 15:10:03 -0400

    Updating the Offcie Services

--- src/app/Services/office.service.ts
+++ src/app/Services/office.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import {HttpClient} from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Office } from '../Interfaces/office';
+
 @Injectable({
   providedIn: 'root'
 })
 export class OfficeService {
 
-  constructor() { }
+  private endpoint:string = environment.endPoint;
+  private apiUrl:string = this.endpoint + "office/";
+
+  constructor(private http:HttpClient) { }
+
+  getList():Observable<Office[]>{
+    return this.http.get<Office[]>(`${this.apiUrl}list`)
+  }
 }
